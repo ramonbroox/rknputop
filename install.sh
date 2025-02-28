a2e37af182f02121ba509208ed295d2a0aaf4f3e
Ramon Poca <48922191+ramonbroox@users.noreply.github.com>
2025-02-28 14:00:21 +0100

    Try pip3 and/or python for #7

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Probably installed by default, but just in case
 sudo apt install python3 python3-pip wget curl -y 
 # Required for python script
-pip install plotext psutil
+pip3 install plotext psutil || python -m pip install plotext psutil 
 
 wget https://raw.githubusercontent.com/ramonbroox/rknputop/main/rknputop
 sudo chmod 755 rknputop
